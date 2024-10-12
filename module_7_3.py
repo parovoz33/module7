d0380ff179cb2d88834a1a8e028ca660a18d4fbc
parovoz33 <parovoz33@MAIL.RU>
2024-10-12 15:47:42 +0300

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -3,22 +3,18 @@ import string
 
 class WordsFinder:
     def __init__(self, *file_names):
-        # Принимаем неограниченное количество файлов и сохраняем их в атрибут file_names
         self.file_names = list(file_names)
 
     def get_all_words(self):
         all_words = {}
-        # Пунктуация, которую нужно убрать
         punctuation = [',', '.', '=', '!', '?', ';', ':', ' - ']
 
         for file_name in self.file_names:
             try:
                 with open(file_name, 'r', encoding='utf-8') as file:
-                    text = file.read().lower()  # Приводим текст к нижнему регистру
-                    # Удаляем пунктуацию
+                    text = file.read().lower()
                     for punct in punctuation:
                         text = text.replace(punct, '')
-                    # Разбиваем текст на слова
                     words = text.split()
                     all_words[file_name] = words
             except FileNotFoundError:
@@ -27,37 +23,33 @@ class WordsFinder:
         return all_words
 
     def find(self, word):
-        word = word.lower()  # Игнорируем регистр при поиске
+        word = word.lower()
         word_positions = {}
-        all_words = self.get_all_words()  # Получаем все слова из файлов
+        all_words = self.get_all_words()
 
         for file_name, words in all_words.items():
             if word in words:
-                word_positions[file_name] = words.index(word) + 1  # Возвращаем позицию (индексация с 1)
+                word_positions[file_name] = words.index(word) + 1
             else:
-                word_positions[file_name] = None  # Если слово не найдено
+                word_positions[file_name] = None
 
         return word_positions
 
     def count(self, word):
-        word = word.lower()  # Игнорируем регистр при подсчете
+        word = word.lower()
         word_count = {}
-        all_words = self.get_all_words()  # Получаем все слова из файлов
+        all_words = self.get_all_words()
 
         for file_name, words in all_words.items():
-            word_count[file_name] = words.count(word)  # Считаем количество вхождений
+            word_count[file_name] = words.count(word)
 
         return word_count
 
 
-# Пример использования:
 finder = WordsFinder('test_file.txt')
 
-# Все слова
 print(finder.get_all_words())
 
-# Поиск слова
 print(finder.find('text'))
 
-# Подсчет вхождений слова
 print(finder.count('text'))
